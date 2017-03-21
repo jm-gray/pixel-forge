88bef4bf6e0a32d60d14d26b6123499cd08042d9
Joshua Gray <jmgray2@Gray-MB.local>
2017-03-20 21:24:44 -0400

    updated var name for consistency with figure scripts

--- KalmanFiltering/KF_error.R
+++ KalmanFiltering/KF_error.R
@@ -33,20 +33,20 @@ error_sample <- sample(1:dim(Y)[1], num_to_sample)
 miss_iter <- 10
 # clusterExport(cl, c("ProgressiveMissingFraction"))
 # clusterEvalQ(cl, {library(reshape2)})
-system.time(total_errors <- parApply(cl, Y[error_sample, ], 1, ProgressiveMissingFraction, landsat_dates=landsat_dates, modis_dates=modis_dates, landsat_sensor=landsat_sensor, cdl_process_sds=cdl_process_sds, cdl_types=cdl_types, miss_iter=miss_iter))
+system.time(tmp <- parApply(cl, Y[error_sample, ], 1, ProgressiveMissingFraction, landsat_dates=landsat_dates, modis_dates=modis_dates, landsat_sensor=landsat_sensor, cdl_process_sds=cdl_process_sds, cdl_types=cdl_types, miss_iter=miss_iter))
 
 # this is the 2nd level of hell, need to preallocate...
 # total_errors <- do.call(rbind, total_errors)
 
 # this is much faster, but not very clean
-errors_dims <- unlist(lapply(total_errors, function(x) dim(x)[1]))
-tmp <- data.frame(matrix(NA, nrow=sum(errors_dims), ncol=dim(total_errors[[1]])[2]))
-names(tmp) <- names(total_errors[[1]])
+errors_dims <- unlist(lapply(tmp, function(x) dim(x)[1]))
+total_errors <- data.frame(matrix(NA, nrow=sum(errors_dims), ncol=dim(tmp[[1]])[2]))
+names(total_errors) <- names(tmp[[1]])
 last_index <- 1
 i <- 1
 for(N in errors_dims){
   print(paste(last_index,":",(last_index + N - 1)))
-  tmp[last_index:(last_index + N - 1), ] <- total_errors[[i]]
+  total_errors[last_index:(last_index + N - 1), ] <- tmp[[i]]
   i <- i + 1
   last_index <- last_index + N
 }
@@ -57,4 +57,4 @@ for(N in errors_dims){
 #=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
 output_directory <- "/share/jmgray2/KF_output"
 output_file <- file.path(output_directory, "kf_error_output_faster.Rdata")
-save(tmp, file=output_file)
+save(total_errors, file=output_file)
