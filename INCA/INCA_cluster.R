3c0e6e0a006b86bff05ea01a805ab139bf6f99d5
Josh Gray <jmgray2@gray-imac.cnr.ncsu.edu>
2019-04-04 13:31:42 -0400

    updated submission info in header of INCA_cluster.R

--- INCA/INCA_cluster.R
+++ INCA/INCA_cluster.R
@@ -1,29 +1,5 @@
-# the sub_INCA_cluster.R script looks like this:
-#
-# #!/bin/csh
-#
-# source /usr/local/apps/R/R-312.csh
-# R --vanilla < /share/jmgray2/INCA_cluster.R --args -tile $1
+# use INCA_suball.sh to submit all jobs
 
-
-# To submit all tiles, we use this script:
-#
-# #!/bin/bash
-#
-# # submit all CONUS tiles as INCA tile summary jobs on henry2
-# SCRATCHDIR="/share/jmgray2/"
-# SCRIPTNAME="sub_INCA_cluster.sh"
-# SCRATCHROOT="INCA"
-#
-# declare -a tiles=("h08v04" "h09v04" "h10v04" "h11v04" "h12v04" "h13v04" "h08v05" "h09v05" "h10v05" "h11v05" "h12v05" "h08v06" "h09v06" "h10v06")
-# for i in "${tiles[@]}"
-# do
-#   bsub_cmd="bsub -q cnr -W 4:00 -n 16 -R \"oc span[ptile=16]\" -o ${SCRATCHDIR}${SCRATCHROOT}.$i.out.%J -e $SCRATCHDIR$SCRATCHROOT.$i.err.%J \"csh ${SCRATCHDIR}${SCRIPTNAME} $i\""
-#   # echo $bsub_cmd
-#   eval $bsub_cmd
-# done
-
-# .libPaths("/home/jmgray2/R/x86_64-unknown-linux-gnu-library/3.1")
 library(raster)
 library(rgdal)
 library(parallel)
@@ -145,6 +121,7 @@ args <- arg_parser$parse_args()
 # make a cluster
 cl <- makeCluster(args$cluster_size)
 clusterExport(cl, c("GetSDS", "PhenoNormals"))
+clusterEvalQ(cl, {library(raster); library(rgdal); library(mblm)})
 
 # compute INCA summaries
 system.time(PhenoNormalsTile(tile=args$tile, cl=cl, data_dir=args$data_dir, output_dir=args$output_dir))
