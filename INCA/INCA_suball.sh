6ba94c2c2f5e75e93f6fb39f3aa9a7d1408e0f64
Josh Gray <jmgray2@gray-imac.cnr.ncsu.edu>
2019-04-30 10:28:39 -0400

    updated run time for INCA_suball.sh

--- INCA/INCA_suball.sh
+++ INCA/INCA_suball.sh
@@ -5,7 +5,7 @@ SCRATCHROOT="INCA"
 declare -a tiles=("h08v04" "h09v04" "h10v04" "h11v04" "h12v04" "h13v04" "h08v05" "h09v05" "h10v05" "h11v05" "h12v05" "h08v06" "h09v06" "h10v06")
 for i in "${tiles[@]}"
 do
-  bsub_cmd="bsub -q cnr -W 4:00 -n 16 -R \"oc span[ptile=16]\" -o ${SCRATCHDIR}${SCRATCHROOT}.$i.out.%J -e $SCRATCHDIR$SCRATCHROOT.$i.err.%J \"csh ${SCRATCHDIR}${SCRIPTNAME} $i\""
+  bsub_cmd="bsub -q cnr -W 12:00 -n 16 -R \"oc span[ptile=16]\" -o ${SCRATCHDIR}${SCRATCHROOT}.$i.out.%J -e $SCRATCHDIR$SCRATCHROOT.$i.err.%J \"csh ${SCRATCHDIR}${SCRIPTNAME} $i\""
   echo $bsub_cmd
 #   eval $bsub_cmd
 done
