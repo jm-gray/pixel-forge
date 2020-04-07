b601f45348d4534e5c9c7ed6f69ff88e60521af6
Josh Gray <jmgray2@gray-imacpro.lan>
2020-04-07 19:11:45 -0400

    added GSL processing option on PhenoNormalsTile(); updated path for R libs

--- INCA/INCA_cluster.R
+++ INCA/INCA_cluster.R
@@ -1,10 +1,15 @@
 # use INCA_suball.sh to submit all jobs
 
-library(raster)
-library(rgdal)
+# library(raster)
+library(raster, lib.loc="/home/jmgray2/libs/R_libs")
+# library(rgdal)
+library(rgdal, lib.loc="/home/jmgray2/libs/R_libs")
 library(parallel)
-library(argparse)
-library(mblm)
+# library(argparse)
+library(argparse, lib.loc="/home/jmgray2/libs/R_libs")
+# library(mblm)
+library(mblm, lib.loc="/home/jmgray2/libs/R_libs")
+
 
 #===============================================================================
 # Functions
@@ -101,8 +106,10 @@ GetSDS <- function(file_path, sds=NULL){
 }
 
 #=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-PhenoNormalsTile <- function(tile, cl, data_dir, output_dir){
+PhenoNormalsTile <- function(tile, cl, data_dir, output_dir, doGSL=FALSE){
   # soup-to-nuts processing of INCA variables for a single tile
+  # added doGSL flag: if this is TRUE, then ONLY growing season length summaries are calculated
+  # these are calculated as MidGreenup to MidGreendown.
 
   # the collection of SDS to calculate normals and trends over
   metrics_to_do <- c("Greenup", "MidGreenup", "Maturity", "Peak", "Senescence", "MidGreendown", "Dormancy", "EVI_Minimum", "EVI_Amplitude", "EVI_Area")
@@ -117,36 +124,75 @@ PhenoNormalsTile <- function(tile, cl, data_dir, output_dir){
   # calculate offset to convert from days since 1970-1-1 to DOY
   doy_offset <- as.integer(as.Date(paste(in_years, "-1-1", sep="")) - as.Date("1970-1-1")) # Jan 1 of each data year as days since 2000-1-1
 
-  for(this_metric in metrics_to_do){
-    # get a RasterStack for all years of this metric
-    pheno_s <- stack(GetSDS(in_files, this_metric))
-    # subset to first layer in each year
-    pheno_s <- subset(pheno_s, seq(1, nlayers(pheno_s), by=2))
-    # set NA value
-    NAvalue(pheno_s) <- 32767
-    # some tiles also have -32768 as an out-of-range value
-    pheno_s[pheno_s == -32768] <- NA
-
-    # convert to DOY if necessary, and set rounding flag
-    if(doy_metric[which(metrics_to_do == this_metric)]){ 
-      pheno_s <- pheno_s - doy_offset
-      round_flag <- TRUE
-    }else{
-      round_flag <- FALSE
-    }
-
-    # get the raw values in a matrix
-    pheno_v <- values(pheno_s)
-
+  # if doing GSL
+  if(doGSL){
+    # get MidGreenup and MidGreendown SDS, calculate their difference in days
+    midgup_s <- stack(GetSDS(in_files, "MidGreenup"))
+    midgup_s <- subset(midgup_s, seq(1, nlayers(midgup_s), by=2))
+    NAvalue(midgup_s) <- 32767
+    midgup_s[midgup_s == -32768] <- NA
+    midgdown_s <- stack(GetSDS(in_files, "MidGreendown"))
+    midgdown_s <- subset(midgdown_s, seq(1, nlayers(midgdown_s), by=2))
+    NAvalue(midgdown_s) <- 32767
+    midgdown_s[midgdown_s == -32768] <- NA
+    gsl_s <- midgdown_s - midgup_s
+    gsl_v <- values(gsl_s)
+    
     # calculate pheno normals, trends, and anomalies
-    pheno_output_v <- parApply(cl, pheno_v, 1, PhenoNormals, round_vals=round_flag)
+    pheno_output_v <- parApply(cl, gsl_v, 1, PhenoNormals, round_vals=TRUE)
 
     # create a blank output raster, assign the values, and write to a file
-    pheno_output_s <- do.call(stack, replicate(dim(pheno_output_v)[1], raster(pheno_s, 1)))
+    pheno_output_s <- do.call(stack, replicate(dim(pheno_output_v)[1], raster(midgup_s, 1)))
     values(pheno_output_s) <- t(pheno_output_v)
-    out_file_name <- file.path(output_dir, paste("INCA", tile, this_metric, "tiff", sep="."))
+    out_file_name <- file.path(output_dir, paste("INCA", tile, "GSL", "tiff", sep="."))
     # writeRaster(file=out_file_name, pheno_output_s)
     writeRaster(file=out_file_name, pheno_output_s, NAflag=32767, overwrite=T)
+
+    # testing...
+    # data_dir <- "/rsstu/users/j/jmgray2/SEAL/INCA/MCD12Q2C6/MCD12Q2"
+    # tile <- "h12v04"
+    # in_files <- dir(data_dir, patt=paste("MCD12.*", tile, ".*hdf$", sep=""), full=T, rec=T)
+    # in_years <- as.integer(gsub(".*A([0-9]{4})001.*$", "\\1", basename(in_files))) # get data years from file name
+    # in_files <- in_files[order(in_years)]
+    # in_years <- sort(in_years)
+    # set.seed(42)
+    # x <- sample(1:nrow(gsl_v), 10)
+    # round_flag=TRUE
+    # pheno_output_v <- apply(gsl_v[x,], 1, PhenoNormals, round_vals=round_flag)
+    # system.time(pheno_output_v <- parApply(cl, gsl_v, 1, PhenoNormals, round_vals=round_flag))
+    
+  }else{
+    for(this_metric in metrics_to_do){
+      # get a RasterStack for all years of this metric
+      pheno_s <- stack(GetSDS(in_files, this_metric))
+      # subset to first layer in each year
+      pheno_s <- subset(pheno_s, seq(1, nlayers(pheno_s), by=2))
+      # set NA value
+      NAvalue(pheno_s) <- 32767
+      # some tiles also have -32768 as an out-of-range value
+      pheno_s[pheno_s == -32768] <- NA
+
+      # convert to DOY if necessary, and set rounding flag
+      if(doy_metric[which(metrics_to_do == this_metric)]){ 
+        pheno_s <- pheno_s - doy_offset
+        round_flag <- TRUE
+      }else{
+        round_flag <- FALSE
+      }
+
+      # get the raw values in a matrix
+      pheno_v <- values(pheno_s)
+
+      # calculate pheno normals, trends, and anomalies
+      pheno_output_v <- parApply(cl, pheno_v, 1, PhenoNormals, round_vals=round_flag)
+
+      # create a blank output raster, assign the values, and write to a file
+      pheno_output_s <- do.call(stack, replicate(dim(pheno_output_v)[1], raster(pheno_s, 1)))
+      values(pheno_output_s) <- t(pheno_output_v)
+      out_file_name <- file.path(output_dir, paste("INCA", tile, this_metric, "tiff", sep="."))
+      # writeRaster(file=out_file_name, pheno_output_s)
+      writeRaster(file=out_file_name, pheno_output_s, NAflag=32767, overwrite=T)
+    }
   }
 }
 
@@ -167,10 +213,14 @@ args <- arg_parser$parse_args()
 # make a cluster
 cl <- makeCluster(args$cluster_size)
 clusterExport(cl, c("GetSDS", "PhenoNormals"))
-clusterEvalQ(cl, {library(raster); library(rgdal); library(mblm)})
+# clusterEvalQ(cl, {library(raster); library(rgdal); library(mblm)})
+clusterEvalQ(cl, {library(raster, lib.loc="/home/jmgray2/libs/R_libs"); library(rgdal, lib.loc="/home/jmgray2/libs/R_libs"); library(mblm, lib.loc="/home/jmgray2/libs/R_libs")})
 
 # compute INCA summaries
-system.time(PhenoNormalsTile(tile=args$tile, cl=cl, data_dir=args$data_dir, output_dir=args$output_dir))
+# system.time(PhenoNormalsTile(tile=args$tile, cl=cl, data_dir=args$data_dir, output_dir=args$output_dir))
+
+# do it just for GSL
+system.time(PhenoNormalsTile(tile=args$tile, cl=cl, data_dir=args$data_dir, output_dir=args$output_dir, doGSL=TRUE))
 
 # # use INCA_suball.sh to submit all jobs
 
--- INCA/INCA_subtile.sh
+++ INCA/INCA_subtile.sh
@@ -1,3 +1,7 @@
 #!/bin/csh
+
+conda activate /usr/local/usrapps/jmgray2/env_gdal
+setenv LD_LIBRARY_PATH /usr/local/usrapps/jmgray2/env_gdal/lib:$LD_LIBRARY_PATH
 module load R
-R --vanilla < /share/jmgray2/INCA_cluster.R --args -tile $1
+
+R --vanilla < /rsstu/users/j/jmgray2/SEAL/INCA/INCA_cluster.R --args -tile $1
